e19767d7880c6f164c955a1fa7c625b9bac52bf1
Olga Serova <58735478+lyolka92@users.noreply.github.com>
2025-08-02 21:27:11 +0300

    Updates

---
+++ assets/text-B9AReDHm.js
@@ -0,0 +1,1 @@
+import{E as t}from"./Root-shJ36WXX.js";const{withContext:e}=t({key:"text"}),x=e("p");export{x as T};
--- assets/useTelegramBackButton-C9DJGfFt.js
+++
@@ -1,1 +0,0 @@
-import{r as a,H as t}from"./index-DHtrePOA.js";import{Q as o}from"./Root-BDc48MYe.js";const c=()=>{const e=o();a.useEffect(()=>(t.show(),t.onClick(()=>e(-1)),()=>{t.hide()}),[])};export{c as u};
