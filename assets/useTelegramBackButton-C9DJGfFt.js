f4fe16827a3f11c80d6f6c5a557254979eb6f426
Olga Serova <58735478+lyolka92@users.noreply.github.com>
2025-08-02 20:53:21 +0300

    Updates

---
+++ assets/useTelegramBackButton-C9DJGfFt.js
@@ -0,0 +1,1 @@
+import{r as a,H as t}from"./index-DHtrePOA.js";import{Q as o}from"./Root-BDc48MYe.js";const c=()=>{const e=o();a.useEffect(()=>(t.show(),t.onClick(()=>e(-1)),()=>{t.hide()}),[])};export{c as u};
