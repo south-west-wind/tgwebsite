1477e1db1453746a40dfa99d4c8c9c47cb2f5e6b
Olga Serova <58735478+lyolka92@users.noreply.github.com>
2025-08-03 13:01:41 +0300

    Updates

---
+++ assets/heading-B-zXRx0U.js
@@ -0,0 +1,1 @@
+import{E as e}from"./Root-CCma-_uu.js";const{withContext:t}=e({key:"heading"}),n=t("h2");export{n as H};
---
+++ assets/text-DsMmS8FH.js
@@ -0,0 +1,1 @@
+import{E as t}from"./Root-CCma-_uu.js";const{withContext:e}=t({key:"text"}),x=e("p");export{x as T};
