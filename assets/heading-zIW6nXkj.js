5ebebb6e0dc54b24c8d769a067b664d855530cb6
Olga Serova <58735478+lyolka92@users.noreply.github.com>
2025-08-03 21:23:54 +0300

    Updates

---
+++ assets/heading-zIW6nXkj.js
@@ -0,0 +1,1 @@
+import{L as e}from"./Root-BH52lSMu.js";const{withContext:t}=e({key:"heading"}),n=t("h2");export{n as H};
---
+++ assets/text-CVxAGaM1.js
@@ -0,0 +1,1 @@
+import{L as t}from"./Root-BH52lSMu.js";const{withContext:e}=t({key:"text"}),x=e("p");export{x as T};
