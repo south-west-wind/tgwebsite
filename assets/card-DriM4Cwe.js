f3d00e41a0118557559199af7e4b17c333df3ec0
Olga Serova <58735478+lyolka92@users.noreply.github.com>
2025-08-03 12:32:15 +0300

    Updates

---
+++ assets/card-DriM4Cwe.js
@@ -0,0 +1,1 @@
+import{c as t}from"./create-slot-recipe-context-Dr_fDWZe.js";const{withProvider:e,withContext:o}=t({key:"card"}),d=e("div","root"),a=o("div","body"),i=o("div","header");o("div","footer");o("h3","title");o("p","description");export{d as C,i as a,a as b};
--- assets/heading-B3fsCNMH.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./Root-Dxl0MaV4.js";const{withContext:t}=e({key:"heading"}),n=t("h2");export{n as H};
