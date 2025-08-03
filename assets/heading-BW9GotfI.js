76d3da64cd3c171f1cabfb07a5e368e5a4dbba66
Olga Serova <58735478+lyolka92@users.noreply.github.com>
2025-08-03 14:39:32 +0300

    Updates

--- assets/heading-BW9GotfI.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./Root-Bvwkfs8t.js";const{withContext:t}=e({key:"heading"}),n=t("h2");export{n as H};
--- assets/text-ldypuAbh.js
+++
@@ -1,1 +0,0 @@
-import{E as t}from"./Root-Bvwkfs8t.js";const{withContext:e}=t({key:"text"}),x=e("p");export{x as T};
