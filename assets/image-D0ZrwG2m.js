3a33dc15ae4bab0b9a3543a7af81288d0fe96773
Olga Serova <58735478+lyolka92@users.noreply.github.com>
2025-08-03 15:11:32 +0300

    Updates

--- assets/image-D0ZrwG2m.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as c}from"./index-BoJusUeh.js";import{y as i,_ as m}from"./Root-BzqaY8BN.js";const g=e.forwardRef(function(a,t){const{align:r,fit:s="cover",...o}=a;return c.jsx(i.img,{ref:t,objectFit:s,objectPosition:r,className:m("chakra-image",a.className),...o})});export{g as I};
