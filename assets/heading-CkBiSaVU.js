230e25c9037c053cb25b5622d695dbaf1c5c25ba
Olga Serova <58735478+lyolka92@users.noreply.github.com>
2025-08-03 12:58:02 +0300

    Updates

--- assets/heading-CkBiSaVU.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./Root-WP6tUMPK.js";const{withContext:t}=e({key:"heading"}),n=t("h2");export{n as H};
---
+++ assets/text-dKUwW1DD.js
@@ -0,0 +1,1 @@
+import{E as t}from"./Root-DwCz7qp-.js";const{withContext:e}=t({key:"text"}),x=e("p");export{x as T};
