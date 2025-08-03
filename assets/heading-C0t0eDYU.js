3f089a18d3920846297b74b28501448815f45f88
Olga Serova <58735478+lyolka92@users.noreply.github.com>
2025-08-03 13:56:24 +0300

    Updates

--- assets/heading-C0t0eDYU.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./Root-BcqswTl4.js";const{withContext:t}=e({key:"heading"}),n=t("h2");export{n as H};
--- assets/text-LfWLSanj.js
+++
@@ -1,1 +0,0 @@
-import{E as t}from"./Root-BcqswTl4.js";const{withContext:e}=t({key:"text"}),x=e("p");export{x as T};
