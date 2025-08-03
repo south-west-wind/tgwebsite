809c60114359ac5ba3d19ed790367536ffae9e46
Olga Serova <58735478+lyolka92@users.noreply.github.com>
2025-08-03 21:07:33 +0300

    Updates

--- assets/heading-BrQO5Glp.js
+++
@@ -1,1 +0,0 @@
-import{L as e}from"./Root-CVN8kiGe.js";const{withContext:t}=e({key:"heading"}),n=t("h2");export{n as H};
---
+++ assets/heading-h7mDP0NI.js
@@ -0,0 +1,1 @@
+import{L as e}from"./Root-BiEBQ71d.js";const{withContext:t}=e({key:"heading"}),n=t("h2");export{n as H};
