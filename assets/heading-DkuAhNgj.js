996e90261b8e6acbcd96e93de7a5b2121d8eba80
Olga Serova <58735478+lyolka92@users.noreply.github.com>
2025-08-04 17:59:05 +0300

    Updates

---
+++ assets/heading-DkuAhNgj.js
@@ -0,0 +1,1 @@
+import{M as e}from"./Root-D0mATJ2G.js";const{withContext:t}=e({key:"heading"}),n=t("h2");export{n as H};
---
+++ assets/text-xPHy-jDO.js
@@ -0,0 +1,1 @@
+import{M as t}from"./Root-D0mATJ2G.js";const{withContext:e}=t({key:"text"}),x=e("p");export{x as T};
