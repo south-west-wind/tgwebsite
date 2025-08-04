705a1cc17621989f41530a916b4d3d60d2dc7ddb
Olga Serova <58735478+lyolka92@users.noreply.github.com>
2025-08-04 17:07:01 +0300

    Updates

--- assets/image-DenyBReA.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as c}from"./index-ObY6if5L.js";import{E as i,H as m}from"./Root-BwfD-xp7.js";const g=e.forwardRef(function(a,t){const{align:r,fit:s="cover",...o}=a;return c.jsx(i.img,{ref:t,objectFit:s,objectPosition:r,className:m("chakra-image",a.className),...o})});export{g as I};
