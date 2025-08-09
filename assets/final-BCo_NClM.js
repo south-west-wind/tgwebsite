8aa1de5f1873c4a0dee1693e9d81aae520acb711
Olga Serova <58735478+lyolka92@users.noreply.github.com>
2025-08-09 14:51:19 +0300

    Updates

---
+++ assets/final-BCo_NClM.js
@@ -0,0 +1,1 @@
+const s="/tgwebsite/assets/final-C2VNx2rl.svg";export{s as f};
--- assets/heading-B5UHVywE.js
+++
@@ -1,1 +0,0 @@
-import{M as e}from"./Root-bPL7dTH0.js";const{withContext:t}=e({key:"heading"}),n=t("h2");export{n as H};
