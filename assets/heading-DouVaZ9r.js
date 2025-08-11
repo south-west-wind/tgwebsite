eec25df70c4b50b237d721aaea8a2ad7f8c724a8
Olga Serova <58735478+lyolka92@users.noreply.github.com>
2025-08-11 19:26:51 +0300

    Updates

---
+++ assets/heading-DouVaZ9r.js
@@ -0,0 +1,1 @@
+import{M as e}from"./Root-B8J9GB6P.js";const{withContext:t}=e({key:"heading"}),n=t("h2");export{n as H};
---
+++ assets/text-Crvh0ron.js
@@ -0,0 +1,1 @@
+import{M as t}from"./Root-B8J9GB6P.js";const{withContext:e}=t({key:"text"}),x=e("p");export{x as T};
